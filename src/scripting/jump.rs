1e3e4e944fa923165f4d2417186d364d85e3986f
Takács Vili <tvili999@gmail.com>
2023-04-16 01:08:22 +0200

    Fixed jump command
     - It was untested and didn't work

--- src/scripting/jump.rs
+++ src/scripting/jump.rs
@@ -17,43 +17,43 @@ impl Command for Jump {
         let mut label = None;
 
         if arguments[0] == "eq" {
-            condition = arguments[0] == arguments[1];
-            label = Some(arguments[2].to_string());
+            condition = arguments[1] == arguments[2];
+            label = Some(arguments[3].to_string());
         }
         if arguments[0] == "ne" {
-            condition = arguments[0] != arguments[1];
-            label = Some(arguments[2].to_string());
+            condition = arguments[1] != arguments[2];
+            label = Some(arguments[3].to_string());
         }
         if arguments[0] == "gt" {
-            condition = arguments[0].parse::<i64>().unwrap() > arguments[1].parse::<i64>().unwrap();
-            label = Some(arguments[2].to_string());
+            condition = arguments[1].parse::<i64>().unwrap() > arguments[2].parse::<i64>().unwrap();
+            label = Some(arguments[3].to_string());
         }
         if arguments[0] == "lt" {
-            condition = arguments[0].parse::<i64>().unwrap() < arguments[1].parse::<i64>().unwrap();
-            label = Some(arguments[2].to_string());
+            condition = arguments[1].parse::<i64>().unwrap() < arguments[2].parse::<i64>().unwrap();
+            label = Some(arguments[3].to_string());
         }
         if arguments[0] == "ge" {
             condition =
-                arguments[0].parse::<i64>().unwrap() >= arguments[1].parse::<i64>().unwrap();
-            label = Some(arguments[2].to_string());
+                arguments[1].parse::<i64>().unwrap() >= arguments[2].parse::<i64>().unwrap();
+            label = Some(arguments[3].to_string());
         }
         if arguments[0] == "le" {
-            condition = arguments[0].parse::<i64>().unwrap() < arguments[1].parse::<i64>().unwrap();
-            label = Some(arguments[2].to_string());
+            condition = arguments[1].parse::<i64>().unwrap() < arguments[2].parse::<i64>().unwrap();
+            label = Some(arguments[3].to_string());
         }
         if arguments[0] == "t" {
-            condition = !(arguments[0].is_empty()
-                || arguments[0] == "0"
-                || arguments[0] == "false"
-                || arguments[0] == "no");
-            label = Some(arguments[1].to_string());
+            condition = !(arguments[1].is_empty()
+                || arguments[1] == "0"
+                || arguments[1] == "false"
+                || arguments[1] == "no");
+            label = Some(arguments[2].to_string());
         }
         if arguments[0] == "f" {
-            condition = arguments[0].is_empty()
-                || arguments[0] == "0"
-                || arguments[0] == "false"
-                || arguments[0] == "no";
-            label = Some(arguments[1].to_string());
+            condition = arguments[1].is_empty()
+                || arguments[1] == "0"
+                || arguments[1] == "false"
+                || arguments[1] == "no";
+            label = Some(arguments[2].to_string());
         }
 
         if let Some(label) = label {
